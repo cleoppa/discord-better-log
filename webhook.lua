eaf5783daf4fbe80085fbff13a89db78e41fa696
Murat Akıllıok <114690203+cleoppa@users.noreply.github.com>
2022-09-29 23:19:04 +0300

    Update webhook.lua

--- webhook.lua
+++ webhook.lua
@@ -1,4 +1,4 @@
-﻿-- Client -- Discord Sunucunuz > Ayarlar > WebHooks Oluştur
+﻿-- cleopatra -- Discord Sunucunuz > Ayarlar > WebHooks Oluştur
 webhooks = {
 	-- isim, isim, webhook linki, avatar url (bot resmi)
 	{"Komutlog", "Komutlog", "https://discord.com/api/webhooks/1021110907713101825/OTH0kYTT7T3luSqlQn9X1k_hhBmMoCB02R2o0lWHHntG2ftYLNe9iFwFbkZxsf5LCmXT", "https://cdn.discordapp.com/attachments/987038766243926118/987039283284156416/logo.png"},
