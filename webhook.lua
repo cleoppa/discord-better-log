d74c584d04b0acdef71d3b63c2f728275ba562f4
Murat Akıllıok <114690203+cleoppa@users.noreply.github.com>
2022-10-02 03:48:49 +0300

    Update webhook.lua

--- webhook.lua
+++ webhook.lua
@@ -1,6 +1,6 @@
--- cleoppa -- Discord Sunucunuz > Ayarlar > WebHooks Oluştur
+-- cleoppa -- Discord Server > Channel Settings > Create Webhooks
 webhooks = {
-	-- isim, isim, webhook linki, avatar url (bot resmi)
-	{"cleopatra", "cleopatra", "https://discord.com/api/webhooks/1021110907713101825/OTH0kYTT7T3luSqlQn9X1k_hhBmMoCB02R2o0lWHHntG2ftYLNe9iFwFbkZxsf5LCmXT", "https://cdn.discordapp.com/attachments/987038766243926118/987039283284156416/logo.png"},
+	-- nickname, nickname, webhook link, avatar url
+	{"cleoppa", "cleoppa", "https://discord.com/api/webhooks/1021110907713101825/OTH0kYTT7T3luSqlQn9X1k_hhBmMoCB02R2o0lWHHntG2ftYLNe9iFwFbkZxsf5LCmXT", "https://cdn.discordapp.com/attachments/987038766243926118/987039283284156416/logo.png"},
 
 }
