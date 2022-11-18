8060ca51d02d9a1461a1173a9be3f5867c310a77
Akilliok <114690203+cleoppa@users.noreply.github.com>
2022-11-18 18:23:57 +0300

    Update webhook.lua

--- webhook.lua
+++ webhook.lua
@@ -1,6 +1,6 @@
 -- cleoppa -- Discord Server > Channel Settings > Create Webhooks
 webhooks = {
 	-- nickname, nickname, webhook link, avatar url
-	{"cleoppa", "cleoppa", "https://discord.com/api/webhooks/1021110907713101825/OTH0kYTT7T3luSqlQn9X1k_hhBmMoCB02R2o0lWHHntG2ftYLNe9iFwFbkZxsf5LCmXT", "https://cdn.discordapp.com/attachments/987038766243926118/987039283284156416/logo.png"},
+	{"cleoppa", "cleoppa", "weebhook number", "https://cdn.discordapp.com/attachments/987038766243926118/987039283284156416/logo.png"},
 
 }
